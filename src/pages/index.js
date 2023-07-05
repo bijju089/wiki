c09814c3db7cf983bc93ad52dd1a6e669b116a65
BijjuXD <103484184+bijju089@users.noreply.github.com>
2023-07-05 15:20:49 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,12 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Docs
+          </Link>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro">
+            Products
           </Link>
         </div>
       </div>
