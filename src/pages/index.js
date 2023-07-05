75cbce26fb14aae0d96f34e49de0867bfebf8296
BijjuXD <103484184+bijju089@users.noreply.github.com>
2023-07-05 15:29:33 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,8 +36,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="The official wiki page of cxSTUDIOS">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
