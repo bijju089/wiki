560479320c03d42c31b1cb857a6e7dc763778500
BijjuXD <103484184+bijju089@users.noreply.github.com>
2023-07-09 11:38:01 +0530

    Update for aurora template

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -92,8 +92,8 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: 'Aurora Nl2',
+                to: '/docs/aurora/intro',
               },
             ],
           },
